b82d5c53c079df39e00afb16d07a93ae95a1d9c9
thony-ui <anthonyhermanto1@gmail.com>
2025-06-24 16:16:56 +0800

    fix linting

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,30 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (
+    config: {
+      resolve: { fallback: any };
+      ignoreWarnings: { module: RegExp; message: RegExp }[];
+    },
+    { isServer }: any
+  ) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    config.ignoreWarnings = [
+      {
+        module: /node_modules\/@supabase\/realtime-js/,
+        message:
+          /Critical dependency: the request of a dependency is an expression/,
+      },
+    ];
 
-const nextConfig: NextConfig = {
-  /* config options here */
+    return config;
+  },
 };
 
 export default nextConfig;
